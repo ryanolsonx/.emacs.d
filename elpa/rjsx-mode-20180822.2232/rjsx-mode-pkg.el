bf76517e3f74fcd13ec44f7fa8bf14655ce9a202
Ryan Olson <rolson@swipeclock.com>
2018-08-23 09:25:06 -0600

    Remove elpa folder

--- elpa/rjsx-mode-20180822.2232/rjsx-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rjsx-mode" "20180822.2232" "Real support for JSX" '((emacs "24.4") (js2-mode "20170504")) :commit "18fec8fcbad9a70d39bc44988682a3a9ce77540a" :keywords '("languages") :authors '(("Felipe Ochoa" . "felipe@fov.space")) :maintainer '("Felipe Ochoa" . "felipe@fov.space") :url "https://github.com/felipeochoa/rjsx-mode/")
