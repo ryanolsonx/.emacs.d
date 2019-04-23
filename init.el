e850b80ff4af40fa26a219e623cbede3780c991a
Ryan Olson <rolson@swipeclock.com>
2019-04-23 07:55:24 -0600

    Remove custom variables comments

--- init.el
+++ init.el
@@ -4,14 +4,6 @@
 (set-default-font "Source Code Pro 16")
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  '(package-selected-packages (quote (company editorconfig magit monokai-theme))))
 (custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
  )
