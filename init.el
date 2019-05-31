ec56aebd3b53f3175c88b5f58092561478997d45
Ryan Olson <rolson@swipeclock.com>
2019-05-31 12:38:42 -0600

    Add melpa and magit package

--- init.el
+++ init.el
@@ -1,9 +1,30 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+(package-refresh-contents)
 (package-initialize)
 
 ;; Syntax highlighting off
 (global-font-lock-mode -1)
 (global-auto-revert-mode t)
 (delete-selection-mode t)
+(fringe-mode 0)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
 
 (setq make-backup-files nil
       backup-inhibited t
@@ -20,5 +41,27 @@
 (add-hook 'scss-mode-hook (lambda () (setq css-indent-offset 2)))
 
 (add-to-list 'auto-mode-alist '("\\.cs" . (lambda ()
-					    (setq tab-width 4
-						  indent-tabs-mode t))))
+                                            (setq tab-width 4
+                                                  indent-tabs-mode t))))
+
+(defun require-package (package)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun require-packages (packages)
+  (mapc #'require-package packages))
+
+(require-packages '(magit))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
