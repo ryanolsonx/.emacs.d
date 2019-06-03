fd683b1c2e44e2876ad1f7e103ff5bfdaecfe778
Ryan Olson <ryolson@me.com>
2019-06-03 15:08:33 -0600

    Simplify emacs config

--- init.el
+++ init.el
@@ -1,53 +1,37 @@
-;; Syntax highlighting off
 (global-font-lock-mode -1)
-
+(set-frame-font "Hack 10" nil t)
 (global-auto-revert-mode t)
-(fringe-mode 0)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+(menu-bar-mode -1)
 
-(setq make-backup-files nil
-      backup-inhibited t
-      auto-save-default nil
-      inhibit-startup-screen t
-      initial-scratch-message ""
-      ring-bell-function 'ignore)
+(setq-default make-backup-files nil
+	      backup-inhibited t
+	      auto-save-default nil
+	      inhibit-startup-screen t
+	      initial-scratch-message ""
+	      ring-bell-function 'ignore)
 
 (require 'sgml-mode)
-(add-hook 'js-mode-hook (lambda () (setq tab-width 2
-					 js-indent-level 2
-					 sgml-basic-offset 2
-					 sgml-attribute-offset 2
-					 indent-tabs-mode nil)
+(add-hook 'js-mode-hook (lambda ()
+			  (setq tab-width 2
+				js-indent-level 2
+				sgml-basic-offset 2
+				sgml-attribute-offset 2
+				indent-tabs-mode nil)
 			  (local-set-key (kbd "RET") 'electric-indent-just-newline)))
 
-(add-hook 'scss-mode-hook (lambda () (setq tab-width 2
-					   css-indent-offset 2
-					   indent-tabs-mode nil)))
+(add-hook 'scss-mode-hook (lambda ()
+			    (setq tab-width 2
+				  css-indent-offset 2
+				  indent-tabs-mode nil)))
 
 (add-to-list 'auto-mode-alist '("\\.cs" . (lambda ()
                                             (setq tab-width 4
                                                   indent-tabs-mode t))))
 
-(defun insert-template (templateFilePath)
-  "Insert a template skeleton from a file path"
-  (interactive (list (read-file-name "Template File Path: " "~/.emacs.d/templates/")))
-  (insert-file-contents templateFilePath))
-
-(global-set-key (kbd "C-c t") 'insert-template)
-
 (when (eq system-type 'windows-nt)
-  (set-frame-font "Consolas 13" nil t)
-
   (defun run-bash ()
     (interactive)
     (let ((shell-file-name "C:\\Program Files\\Git\\bin\\bash.exe"))
-      (shell "*bash*")))
-
-  (defun run-cmd ()
-    (interactive)
-    (let ((shell-file-name "cmd.exe"))
-      (shell "*cmd.exe*"))))
-
-(when (eq system-type 'darwin)
-  (set-frame-font "Hack 15" nil t))
+      (shell "*bash*"))))
