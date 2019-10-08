81cd92ae10e320c350ade6e95603ee6d0ca3710b
Ryan Olson <ryanolsonx@gmail.com>
2019-10-08 08:23:49 -0600

    Clean up

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+(when (display-graphic-p)
+  (load "~/.emacs.d/gui.el"))
 (global-font-lock-mode -1)
 (column-number-mode t)
 (savehist-mode)
@@ -7,6 +9,8 @@
               js-indent-level 2
               css-indent-offset 2
               vc-follow-symlinks t)
-(add-to-list 'auto-mode-alist '("\\.cs" . (lambda () (setq tab-width 4 indent-tabs-mode t))))
-(add-hook 'js-mode-hook '(lambda () (local-set-key (kbd "M-.") 'xref-find-definitions)))
-
+(add-to-list 'auto-mode-alist '("\\.cs" . (lambda ()
+                                            (setq tab-width 4
+                                                  indent-tabs-mode t))))
+(add-hook 'js-mode-hook (lambda ()
+                          (local-set-key (kbd "M-.") 'xref-find-definitions)))
