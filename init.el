177c3288ec048dbaafddf15df7ed5015e63bea16
Ryan Olson <ryanolsonx@gmail.com>
2019-10-14 10:00:55 -0600

    Empty init and connect to melpa

---
+++ init.el
@@ -0,0 +1,17 @@
+(require 'package)
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  (when no-ssl
+    (warn "\
+Your version of Emacs does not support SSL connections,
+which is unsafe because it allows man-in-the-middle attacks.
+There are two things you can do about this warning:
+1. Install an Emacs version that does support SSL and be safe.
+2. Remove this warning from your init file so you won't see it again."))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t))
+
+(package-initialize)
+
+(global-set-key (kbd "C-x g") 'magit-status)
