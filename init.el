a0678c1f82afe2ef2b8e5bc3c735c80d5a4dc679
Ryan Olson <ryanolsonx@gmail.com>
2020-02-03 11:17:17 -0700

    Strip away more

--- init.el
+++ init.el
@@ -1,16 +1,8 @@
-(when (display-graphic-p)
-  (load "~/.emacs.d/gui.el"))
-(global-font-lock-mode -1)
-(column-number-mode t)
 (savehist-mode)
 (global-auto-revert-mode t)
 (setq-default indent-tabs-mode nil
               tab-width 2
               js-indent-level 2
-              css-indent-offset 2
-              vc-follow-symlinks t)
-(add-to-list 'auto-mode-alist '("\\.cs" . (lambda ()
-                                            (setq tab-width 4
-                                                  indent-tabs-mode t))))
-(add-hook 'js-mode-hook (lambda ()
-                          (local-set-key (kbd "M-.") 'xref-find-definitions)))
+              css-indent-offset 2)
+(add-hook 'js-mode-hook '(lambda () (local-set-key (kbd "M-.") 'xref-find-definitions)))
+
