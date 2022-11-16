f01a3495a6208fdf3e1c43cbe4a4d18826cec5af
Ryan Olson <rolson@aumni.fund>
2022-11-16 10:18:33 -0700

    Configure theme to match VS Light+

--- init.el
+++ init.el
@@ -45,3 +45,22 @@
 (add-to-list 'auto-mode-alist '("\\.ts\\'" . js-mode))
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right. 
+ '(font-lock-builtin-face ((t (:foreground "#AF00DB"))))
+ '(font-lock-comment-face ((t (:foreground "#008000"))))
+ '(font-lock-constant-face ((t (:foreground "#0000FF"))))
+ '(font-lock-function-name-face ((t (:foreground "#267F99"))))
+ '(font-lock-keyword-face ((t (:foreground "#0000FF"))))
+ '(font-lock-string-face ((t (:foreground "#A31515"))))
+ '(font-lock-type-face ((t (:foreground "#795E26"))))
+ '(font-lock-variable-name-face ((t (:foreground "#0070C1")))))
