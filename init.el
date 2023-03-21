b132904165859b1070b1d001b2f98a0894a595da
Ryan Olson <rolson@aumni.fund>
2023-03-21 11:37:00 -0600

    Add elm-mode

--- init.el
+++ init.el
@@ -39,12 +39,14 @@
 (use-package zenburn-theme
   :config (load-theme 'zenburn t))
 
+(use-package elm-mode)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(zenburn-theme magit use-package)))
+ '(package-selected-packages '(elm-mode zenburn-theme magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
