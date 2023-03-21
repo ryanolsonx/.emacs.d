b779689122daa63eed716b8a136b2a9a66bf8667
Ryan Olson <rolson@aumni.fund>
2023-03-21 11:18:58 -0600

    Install Magit

--- init.el
+++ init.el
@@ -8,12 +8,15 @@
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
+;; -- PACKAGES
+(use-package magit)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages '(magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
