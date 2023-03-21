56b5fe8f6c98c3ccd913bf93dba2518dd02d1d55
Ryan Olson <rolson@aumni.fund>
2023-03-21 11:24:39 -0600

    Install Zenburn theme

--- init.el
+++ init.el
@@ -24,12 +24,15 @@
 ;; -- PACKAGES
 (use-package magit)
 
+(use-package zenburn-theme
+  :config (load-theme 'zenburn t))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit use-package)))
+ '(package-selected-packages '(zenburn-theme magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
