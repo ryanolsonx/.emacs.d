1eabc4a827711486eec2d51edbb92e657f8808e5
Ryan Olson <rolson@aumni.fund>
2023-03-23 22:50:33 -0600

    Latest

--- init.el
+++ init.el
@@ -1,8 +1,5 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 (require 'use-package-ensure)
@@ -16,9 +13,10 @@
 (delete-selection-mode t)
 (column-number-mode t)
 (savehist-mode t)
+(global-linum-mode t)
 
 ;; -- SETTINGS
-(set-frame-font "Menlo 18" nil t)
+(set-frame-font "Menlo 16" nil t)
 (setq-default inhibit-startup-screen t
 	      ring-bell-function 'ignore
 	      indent-tabs-mode nil
@@ -32,6 +30,7 @@
 (global-set-key (kbd "C-2") (lambda () (interactive) (split-window-vertically) (other-window 1)))
 (global-set-key (kbd "C-3") (lambda () (interactive) (split-window-horizontally) (other-window 1)))
 (global-set-key (kbd "C-o") (lambda () (interactive) (other-window -1)))
+(global-set-key (kbd "C-,") 'beginning-of-buffer)
 
 ;; -- PACKAGES
 (use-package magit
@@ -39,6 +38,8 @@
 
 (use-package zenburn-theme
   :config (load-theme 'zenburn t))
+
+(use-package doom-themes)
 
 (use-package elm-mode)
 
@@ -59,45 +60,54 @@
   (setq lsp-ui-sideline-enable t)
   (setq lsp-ui-doc-enable t)
   (setq lsp-ui-peek-enable t)
-  (setq lsp-ui-peek-always-show t)
-  :hook js-mode)
+  (setq lsp-ui-peek-always-show t))
 
 (use-package which-key
   :config (which-key-mode))
 
+(use-package vterm)
+
+(use-package vterm-toggle
+  :config (global-set-key (kbd "C-c t") 'vterm-toggle))
+
+(use-package counsel
+  :config
+  (global-set-key (kbd "C-c f") 'counsel-git)
+  (global-set-key (kbd "C-c o") 'counsel-outline))
+
 ;; -- VIM
 
-(use-package evil
-  :init (setq evil-shift-width 2
-              evil-want-keybinding nil
-              evil-want-C-u-delete t
-              evil-want-C-u-scroll t
-              evil-want-C-w-in-emacs-state t)
-  :config (evil-mode 1))
-
-(use-package evil-collection
-  :after evil
-  :ensure t
-  :config
-  (evil-collection-init))
-
-;; Ensure that ESC quits things
-(defun minibuffer-keyboard-quit ()
-  "Abort recursive edit.
-In Delete Selection mode, if the mark is active, just deactivate it;
-then it takes a second \\[keyboard-quit] to abort the minibuffer."
-  (interactive)
-  (if (and delete-selection-mode transient-mark-mode mark-active)
-      (setq deactivate-mark  t)
-    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
-    (abort-recursive-edit)))
-(define-key evil-normal-state-map [escape] 'keyboard-quit)
-(define-key evil-visual-state-map [escape] 'keyboard-quit)
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+;;(use-package evil
+;;  :init (setq evil-shift-width 2
+;;              evil-want-keybinding nil
+;;              evil-want-C-u-delete t
+;;              evil-want-C-u-scroll t
+;;              evil-want-C-w-in-emacs-state t)
+;;  :config (evil-mode 1))
+;;
+;;(use-package evil-collection
+;;  :after evil
+;;  :ensure t
+;;  :config
+;;  (evil-collection-init))
+;;
+;;;; Ensure that ESC quits things
+;;(defun minibuffer-keyboard-quit ()
+;;  "Abort recursive edit.
+;;In Delete Selection mode, if the mark is active, just deactivate it;
+;;then it takes a second \\[keyboard-quit] to abort the minibuffer."
+;;  (interactive)
+;;  (if (and delete-selection-mode transient-mark-mode mark-active)
+;;      (setq deactivate-mark  t)
+;;    (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
+;;    (abort-recursive-edit)))
+;;(define-key evil-normal-state-map [escape] 'keyboard-quit)
+;;(define-key evil-visual-state-map [escape] 'keyboard-quit)
+;;(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+;;(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+;;(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+;;(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+;;(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -105,7 +115,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(which-key lsp-mode company company-mode prettier-js add-node-modules-path exec-path-from-shell evil-collection evil elm-mode zenburn-theme magit use-package)))
+   '(doom-themes vterm-toggle counsel vterm which-key lsp-mode company company-mode prettier-js add-node-modules-path exec-path-from-shell evil-collection evil elm-mode zenburn-theme magit use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
