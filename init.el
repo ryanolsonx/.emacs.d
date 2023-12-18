566d7a0c344b21ca801457dc88daab960064a724
Ryan Olson <rolson@aumni.fund>
2023-12-18 12:48:57 -0700

    latest

--- init.el
+++ init.el
@@ -5,6 +5,12 @@
 (column-number-mode t)
 (savehist-mode t)
 
+(set-background-color "#232323")
+(set-foreground-color "#c5b19b")
+;;(set-face-attribute 'font-lock-keyword-face nil :foreground "#267a78")
+;;(set-face-attribute 'font-lock-comment-face nil :foreground "#a38621")
+;;(set-face-attribute 'font-lock-type-face nil :foreground "#abcd74")
+
 (setq-default inhibit-startup-screen t
 	      ring-bell-function 'ignore
 	      indent-tabs-mode nil
@@ -23,24 +29,24 @@
 (global-set-key (kbd "C-.") 'end-of-buffer)
 (global-set-key (kbd "C-c g") 'magit-status)
 
-;; (set-cursor-color "#abcd74")
+(set-cursor-color "#abcd74")
 
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(default ((t (:inherit nil :extend nil :stipple nil :background "#2d2925" :foreground "#c5b19b" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 160 :width normal :foundry "nil" :family "Menlo"))))
-;;  '(font-lock-builtin-face ((t (:foreground "#e7c173"))))
-;;  '(font-lock-comment-face ((t (:foreground "#abcd74"))))
-;;  '(font-lock-constant-face ((t (:foreground "#e7c173"))))
-;;  '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
-;;  '(font-lock-function-name-face ((t nil)))
-;;  '(font-lock-keyword-face ((t (:foreground "#fadfc2"))))
-;;  '(font-lock-string-face ((t (:foreground "#df8d68"))))
-;;  '(font-lock-type-face ((t (:foreground "#e7c173"))))
-;;  '(font-lock-variable-name-face ((t nil)))
-;;  '(org-block ((t (:extend t)))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2d2925" :foreground "#c5b19b" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Menlo"))))
+ '(font-lock-builtin-face ((t (:foreground "#e7c173"))))
+ '(font-lock-comment-face ((t (:foreground "#abcd74"))))
+ '(font-lock-constant-face ((t (:foreground "#e7c173"))))
+ '(font-lock-doc-face ((t (:inherit font-lock-comment-face))))
+ '(font-lock-function-name-face ((t nil)))
+ '(font-lock-keyword-face ((t (:foreground "#fadfc2"))))
+ '(font-lock-string-face ((t (:foreground "#df8d68"))))
+ '(font-lock-type-face ((t (:foreground "#e7c173"))))
+ '(font-lock-variable-name-face ((t nil)))
+ '(org-block ((t (:extend t)))))
 ;; (custom-set-variables
 ;;  ;; custom-set-variables was added by Custom.
 ;;  ;; If you edit it by hand, you could mess it up, so be careful.
